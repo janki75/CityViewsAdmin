8d92a09cb6af7d0ba56d5384f4efc42aa67c49c5
Greha shah <grehashah56@gmail.com>
2020-11-26 15:48:03 +0530

    fund done

--- src/app/views/election/election.component.ts
+++ src/app/views/election/election.component.ts
@@ -8,15 +8,61 @@ import { ElectionService } from '../../services/election.service';
 })
 export class ElectionComponent implements OnInit {
   electionDetail:any[] = [];
+  electionName : any;
+  err : string;
+  electionId : any;
+  date : any;
+  eleDate : any;
+  todaydt : any;
+  year:any;
   constructor(private ele : ElectionService) { }
 
   ngOnInit() {
     this.ele.getallelection()
     .subscribe((res:any) => {
       this.electionDetail = res;
-      console.log(res);
     })
 
   }
 
+  getDetails(data){
+    this.electionName = data.electionName;
+    this.electionId = data.id;
+    this.date = data.date;
+  }
+
+  updateEleDetails(data){
+    const data1 = {
+      electionId : this.electionId,
+      date : this.date,
+      electionName : this.electionName
+    }
+    this.ele.updateElectionName(data1)
+    .subscribe((res) => {
+
+    })
+    this.err = "Election heading is updated successfully!!";
+  }
+
+  /*addEleDetails(data){
+    this.todaydt=new Date().toString();
+    var month=new Date().getMonth()+1;
+    this.year=new Date().getFullYear().toString();
+    
+        console.log(eleDate > date);
+    if(eleDate >= date){
+      const data1 = {
+        electionName : this.electionName,
+        date : this.eleDate 
+      }
+      this.ele.addElectionDetail(data1)
+      .subscribe((res) => {
+  
+      })
+      this.err = "Election is arranged successfully!!";
+    }
+    else{
+      this.err = "The date must be greater than current date!!";
+    }
+  }*/
 }
